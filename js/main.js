4768563d67c4995c81bb1ba854b9177ac2b592a9
siga44 <61683171+siga44@users.noreply.github.com>
2020-04-22 13:13:57 +0500

    fixed adaptive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 let body = document.querySelector("body");
 let burger = body.querySelector(".burger");
 let menu = body.querySelector(".menu");
+let input = body.querySelector(".subscribe__input");
 burger.onclick = () => {
   burger.classList.toggle("burger-active");
   menu.classList.toggle("menu-active");
   body.classList.toggle("lock");
+};
+
+input.onfocus = () => {
+  input.toggleAttribute("placeholder");
 };
